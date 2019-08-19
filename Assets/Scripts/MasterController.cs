ad11ef0a3cc1b16c3d8322bca4d00ab09838fd0e
geogeo20 <ifrimgrigorie99@outlook.com>
2019-08-19 16:23:20 +0300

    Initial commit

---
+++ Assets/Scripts/MasterController.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MasterController : MonoBehaviour
+{
+
+    public GameObject tile;
+
+    private static int w = 10;
+    private static int h = 18;
+
+    private static int axes = 2;
+    private static int directions = 10;
+
+    private int[,] _dir = new int[axes, directions];
+
+
+    public static TileController[,] tiles = new TileController[w,h];
+
+    public static bool isGameOver = true;
+
+    private float holdTime = 0.8f;
+    private float acumTime = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        for(float i=0 ; i< 10; i++)
+            for(float j=0; j<18; j++)
+                    Instantiate(tile, new Vector3(i,j,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene("MineSweeper");
+
+
+
+        
+        if(Input.touchCount > 0)
+        {
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+            bool tapDone = false;
+
+            acumTime += Input.GetTouch(0).deltaTime;
+
+            if (acumTime >= holdTime && !tapDone)
+            {
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.name);
+                    hit.collider.gameObject.SendMessage("MakeQuestion");
+                    tapDone = !tapDone;
+                    acumTime = 0;
+                    Handheld.Vibrate();
+                }
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended && acumTime < holdTime && !tapDone)
+            {
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.name);
+                    hit.collider.gameObject.SendMessage("TouchDown");
+                    acumTime = 0;
+                    tapDone = !tapDone;
+                }
+            }
+
+        }
+
+
+        /* if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+                hit.collider.gameObject.SendMessage("TouchDown");
+            }
+        }*/
+            
+
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+    }
+
+    public static void UncoverMines()
+    {
+
+        foreach (TileController tile in tiles)
+        {
+            int x = (int) tile.transform.position.x;
+            int y = (int) tile.transform.position.x;
+
+
+            if (tile.isMine)
+                tile.LoadTexture();
+            else
+                tile.LoadTexture();
+        }
+    }
+
+    public static void UncoverBlanks(int x, int y)
+    {
+        
+        
+        if (tiles[x, y].isUncovered || tiles[x,y].isMine )
+            return;
+
+        tiles[x,y].LoadTexture();
+
+        if (SetCount(x, y) > 0)
+            return;
+
+
+        if( y < h-1) UncoverBlanks(x,y+1);
+        if( y != 0) UncoverBlanks(x, y-1);
+        if( x < w-1) UncoverBlanks(x+1, y);
+        if( x != 0) UncoverBlanks(x-1, y);
+        if( x < w-1 && y < h-1) UncoverBlanks(x+1,y+1);
+        if( x < w-1 && y != 0) UncoverBlanks(x+1,y-1);
+        if( x != 0 && y != 0 ) UncoverBlanks(x-1, y-1);
+        if( x != 0 && y < h-1) UncoverBlanks(x-1,y+1);
+    }
+    
+    
+    
+    public static void GameOver()
+    {
+        // isGameOver = false;
+    }
+    
+
+    public static bool mineAt(int x, int y)
+    {
+        return tiles[x, y].isMine;
+    }
+    
+    public static int SetCount(int x, int y)
+    {
+        int count = 0;
+        
+        if( y < h-1 ) if (mineAt(x, y + 1)) ++count; 
+        if( x < w-1 && y < h-1) if (mineAt(x + 1, y + 1)) ++count;
+        if( x < w-1) if (mineAt(x + 1, y)) ++count;
+        if( x < w-1 && y != 0)if (mineAt(x + 1, y - 1)) ++count;
+        if( y != 0) if (mineAt(x, y - 1)) ++count;
+        if( x != 0 && y != 0 ) if (mineAt(x - 1, y - 1)) ++count;
+        if( x != 0)if (mineAt(x - 1, y)) ++count;
+        if( x != 0 && y < h-1) if (mineAt(x - 1, y + 1)) ++count;
+        
+
+        return count;
+    }
+    
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
+using UnityEngineInternal;
+using Random = UnityEngine.Random;
+
+public class TileController : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public Sprite mine;
+    public Sprite qustion;
+    public Sprite defaultSprite;
+
+    public int setCount;
+    
+    public bool isMine;
+
+    public bool isUncovered = false;
+    // Start is called before the first frame update
+
+
+    private bool isQuestion = false;
+
+    void Start()
+    {
+        isMine = Random.value < 0.20;
+        var position = transform.position;
+        int x = (int) position.x;
+        int y = (int) position.y;
+      
+
+        MasterController.tiles[x, y] = this;
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // TouchMovement();
+        if (Input.GetMouseButton(1))
+            MakeQuestion();
+            
+        
+    }
+
+    public void MakeQuestion()
+    {
+        if(!isUncovered)
+        {
+            if (isQuestion == false)
+
+            {
+                GetComponent<SpriteRenderer>().sprite = qustion;
+                isQuestion = !isQuestion;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = defaultSprite;
+                isQuestion = !isQuestion;
+            }
+        }
+        
+            
+       
+    }
+
+    public void LoadTexture()
+    {
+        int x = (int) transform.position.x;
+        int y = (int) transform.position.y;
+        if(isMine)
+            GetComponent<SpriteRenderer>().sprite = mine;
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[MasterController.SetCount(x,y)];
+            isUncovered = true;
+        }
+        
+
+        
+    }
+
+    public bool IsCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "Default";
+    }
+
+
+    private void TouchMovement()
+    {
+        if (Input.touchCount <= 0 || Input.GetTouch(0).phase != TouchPhase.Began) return;
+        if (isMine)
+        {
+            MasterController.UncoverMines();
+            MasterController.GameOver();
+            print("You lose!");
+        }
+        else
+        {
+            int x = (int) transform.position.x;
+            int y = (int) transform.position.y;
+            LoadTexture();
+            if (MasterController.SetCount(x, y) == 0)
+                MasterController.UncoverBlanks(x, y);
+            print(MasterController.SetCount(x, y));
+        }
+    }
+
+    public void TouchDown()
+    {
+        if (isMine && !isQuestion)
+        {
+
+            MasterController.UncoverMines();
+            MasterController.GameOver();
+            print("You lose!");
+        }
+        else if( isQuestion == false)
+        {
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+
+            if (MasterController.SetCount(x, y) == 0)
+            {
+                print("Bagami-as pla in mata de methoda infecta");
+                MasterController.UncoverBlanks(x, y);
+            }
+            else
+                LoadTexture();
+            print(MasterController.SetCount(x, y));
+        }
+    }
+
+    /*
+    private void OnMouseUp()
+    {
+        if (isMine)
+        {
+
+            MasterController.UncoverMines();
+            MasterController.GameOver();
+            print("You lose!");
+        }
+        else
+        {
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+
+            if (MasterController.SetCount(x, y) == 0)
+            {
+                print("Bagami-as pla in mata de methoda infecta");
+                MasterController.UncoverBlanks(x, y);
+            }
+            else
+                LoadTexture();
+            print(MasterController.SetCount(x, y));
+        }
+        
+    }
+    */
+
+
+
+
+    private void OnMouseDown()
+    {
+        // if(MasterController.isGameOver)
+        
+    }
+    
+}
